fdf1a82d18e5688b51520b5006749ecf2c012bc2
Kratus <34885978+fgames9000@users.noreply.github.com>
2022-12-22 19:00:51 -0300

    Camus update 2
    - Added the animation filter for the ice tint effect
    - Added the ice tint effect to Camus character

--- scripts/camus_ice.c
+++ scripts/camus_ice.c
@@ -6,9 +6,18 @@ void main()
 void iceEffect(){
 	void self	= getlocalvar("self");
 	void target	= getlocalvar("damagetaker");
+	void anim	= getentityproperty(self, "animationID");
 	float time	= openborvariant("elapsed_time");
-	float mult	= 1;
-	float limit = 200*mult;
 
-	setentityvar(target, "iceTime", time+limit);
+	if(	anim == openborconstant("ANI_IDLE")			|| //THIS ONE IS USED ONLY BY THE ICE STAR ENTITY
+		anim == openborconstant("ANI_SPECIAL")		||
+		anim == openborconstant("ANI_SPECIAL2")		||
+		anim == openborconstant("ANI_RUNATTACK")	||
+		anim == openborconstant("ANI_FREESPECIAL")	||
+		anim == openborconstant("ANI_FREESPECIAL6")	){
+		
+		float mult	= 1;
+		float limit = 200*mult;
+		setentityvar(target, "iceTime", time+limit);
+	}
 }
