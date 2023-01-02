0e12bb66aece42f4cec62668b924d06074ae6388
Gilou9999 <gilou9999@gmail.com>
2023-01-02 22:52:29 +0100

    Seiya fixes (partial) since darknior and shiva returns

--- scripts/didhit/tint_silver.c
+++ scripts/didhit/tint_silver.c
@@ -5,7 +5,6 @@ void main()
 	tintEffect(openborconstant("ANI_IDLE"), "silver", 1);
 	tintEffect(openborconstant("ANI_RUNATTACK"), "silver", 1);
 	tintEffect(openborconstant("ANI_SPECIAL"), "silver", 1);
-     tintEffect(openborconstant("ANI_ATTACK5"), "silver", 1);
 	tintEffect(openborconstant("ANI_ATTACKBOTH"), "silver", 1);
 	tintEffect(openborconstant("ANI_FREESPECIAL5"), "silver", 1);
 
