4ebd454cf1f97e1ff77428cca8b42fb6b4ad89c1
Gilou9999 <gilou9999@gmail.com>
2023-01-10 19:20:33 +0100

    Shiryu updates (back attack added, melee attack, commands correctly mapped now)

--- scripts/didhit/tint_green.c
+++ scripts/didhit/tint_green.c
@@ -6,6 +6,7 @@ void main()
 	tintEffect(openborconstant("ANI_RUNATTACK"), "green", 1);
 	tintEffect(openborconstant("ANI_SPECIAL"), "green", 1);
 	tintEffect(openborconstant("ANI_SPECIAL2"), "green", 1);
+     tintEffect(openborconstant("ANI_ATTACKBOTH"), "green", 1);
 	tintEffect(openborconstant("ANI_FREESPECIAL"), "green", 1);
 	tintEffect(openborconstant("ANI_FREESPECIAL2"), "green", 1);
 	tintEffect(openborconstant("ANI_FREESPECIAL3"), "green", 1);
