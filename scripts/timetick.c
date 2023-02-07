22dca1f39ffc6fe79b89e3d240f7ac3683283ccc
Kratus <34885978+fgames9000@users.noreply.github.com>
2023-02-07 20:35:38 -0300

    Minor fix on the reset level timer
    - Now the script will check if there's any player active on the screen, otherwise proceed to the normal countdown for the game over screen

--- scripts/timetick.c
+++ scripts/timetick.c
@@ -1,8 +1,10 @@
 void main()
-{
+{//Reset the current level if the timer ends
 	void branch	= openborvariant("current_branch");
+	int players	= openborvariant("count_players");
 	float time	= openborvariant("game_time");
 	float limit	= 0.5;
 
-	if(time <= limit){jumptobranch(branch, 1);}
+	//RESTART THE CURRENT LEVEL ONLY IF THERE'S ANY PLAYER ON THE SCREEN, OTHERWISE PROCEED TO THE NORMAL GAME OVER
+	if(time <= limit && players > 0){jumptobranch(branch, 1);}
 }
