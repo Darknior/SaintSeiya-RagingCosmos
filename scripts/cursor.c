8854411e52866d776dbcba741777c4ac7c16c21f
Kratus <34885978+fgames9000@users.noreply.github.com>
2023-07-01 06:26:03 -0300

    "Lowercased" the seiya name
    - And remove an unused script

--- scripts/cursor.c
+++ scripts/cursor.c
@@ -48,42 +48,4 @@ void main()
 			setlocalvar("drawmethod"+self, NULL());
 		}
 	}
-	
-	//CPU PARTNER CURSOR HIGHLIGHT
-	if(ani == openborconstant("ANI_FOLLOW17")){ //PARTNER
-	
-		if(time%2 == 0){ //USED TO SAVE CPU POWER
-			if(getlocalvar("drawmethod"+self) == NULL()){setlocalvar("drawmethod"+self, 1);}
-			if(getlocalvar("color") == NULL()){setlocalvar("color", 0xFF);}
-			
-			void operation	= getlocalvar("operation");
-			float color		= getlocalvar("color");
-			float rate		= 4;
-			int tintMode	= 6;
-			int maxLimit	= 200;
-			int minLimit	= 20;
-			
-			if(color != NULL()){
-				changedrawmethod(self, "enabled", 1);
-				changedrawmethod(self, "tintmode", tintMode);
-				changedrawmethod(self, "tintcolor", rgbcolor(color,color,color));
-				
-				//CHANGE OPERATION WHEN REACHED THE MIN/MAX LIMITS
-				if(color >= maxLimit){setlocalvar("operation", "decrease");}else
-				if(color <= minLimit){setlocalvar("operation", "increase");}
-				
-				//INCREASE OR DECREASE COLOR VALUE
-				if(operation == "increase"){setlocalvar("color", color+rate);}else
-				if(operation == "decrease"){setlocalvar("color", color-rate);}
-			}
-		}
-	}
-	
-	//CPU PARTNER CURSOR SELECTED
-	if(ani == openborconstant("ANI_FOLLOW18")){ //PARTNER
-		if(getlocalvar("drawmethod"+self) != NULL()){
-			changedrawmethod(self, "enabled", 0);
-			setlocalvar("drawmethod"+self, NULL());
-		}
-	}
 }
