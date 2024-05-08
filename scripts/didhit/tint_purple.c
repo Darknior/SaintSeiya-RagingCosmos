79219a796747020184d965d6fcc092ba5d01aa95
Gilou9999 <gilou9999@gmail.com>
2024-05-08 18:37:50 +0200

    update of shun moves, he is no longer a shitty character now ;)

--- scripts/didhit/tint_purple.c
+++ scripts/didhit/tint_purple.c
@@ -6,6 +6,7 @@ void main()
 	tintEffect(openborconstant("ANI_RUNATTACK"), "purple", 1);
 	tintEffect(openborconstant("ANI_SPECIAL"), "purple", 1);
      tintEffect(openborconstant("ANI_ATTACK6"), "purple", 1);
+     tintEffect(openborconstant("ANI_ATTACK10"), "purple", 1);
 	tintEffect(openborconstant("ANI_SPECIAL2"), "purple", 1);
 	tintEffect(openborconstant("ANI_FREESPECIAL"), "purple", 1);
 	tintEffect(openborconstant("ANI_FREESPECIAL2"), "purple", 1);
